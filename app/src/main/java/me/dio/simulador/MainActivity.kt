d0d0a01e104940008db35cd3a9c3c332082ce533
jairft <jairpg1993@gmail.com>
2022-06-06 16:57:07 -0300

    desenvolvimento de layouts de listagem de partidas

--- app/src/main/java/me/dio/simulador/MainActivity.kt
+++ app/src/main/java/me/dio/simulador/MainActivity.kt
@@ -1,11 +1,21 @@
 package me.dio.simulador
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
     }
 }
